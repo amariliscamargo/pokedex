e901e1c2d235f53e2fcfb7523e7c0e40a5fa4fd5
Amarilis Camargo <amariliscamargo@gmail.com>
2022-06-17 18:10:58 -0300

    Adicionada as páginas about e index e seus respectivos estilos
    Signed-off-by: Amarilis Camargo <amariliscamargo@gmail.com>

--- pages/about.js
+++ pages/about.js
@@ -3,10 +3,10 @@ import Image from 'next/image'
 
 const About = () => {
   return (
-    <div>
+    <div className={styles.about}>
       <h1>Sobre o projeto</h1>
       <p>Nullam ullamcorper, neque non hendrerit pulvinar, nulla arcu auctor ligula, vitae viverra quam ipsum at enim. Fusce vitae cursus purus. Praesent laoreet et tortor a accumsan. Integer quam felis, convallis sit amet fringilla at, euismod sed justo.</p>
-      <Image src={'/images/charizard.png'} width='' height='' alt=''/>
+      <Image src={'/images/charizard.png'} width='300' height='300' alt='Charizard'/>
     </div>
   )
 }

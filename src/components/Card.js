8ac24f25d3a977f4093d8f30ca21263f5072d6f2
Amarilis Camargo <amariliscamargo@gmail.com>
2022-06-23 14:42:46 -0300

    Finalizando o projeto

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,18 @@
-const Card = () => {
+import Image from "next/image"
+import Link from "next/link"
+
+import styles from '../../styles/components/Card.module.css'
+
+const Card = ({pokemon}) => {
   return (
-    <div>Card</div>
+    <div className={styles.card}>
+      <Image src={`https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`} width="120" height="120" alt={pokemon.name}/> 
+      <p className={styles.id}>#{pokemon.id}</p>
+      <h3 className={styles.title}>{pokemon.name}</h3>
+      <Link href={`/pokemon/${pokemon.id}`}>
+        <a className={styles.btn}>detalhes</a>
+      </Link>
+    </div>
   )
 }
 
